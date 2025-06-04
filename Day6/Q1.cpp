2b0389c8b3a87769dff34af823f15cf7ca0b4c95
nishant <nishantsharma30305@gmail.com>
2025-06-04 21:37:37 +0530

    Day6 solutions

---
+++ Day6/Q1.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution{
+    public:
+    int missingNo(vector<int>&num,vector<int>&arr,int n){
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-1;j++){
+                num[j]!=arr[i];
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+};
+
+int main(){
+    int n;
+    cout<<"enter the integar: ";
+    cin>>n;
+    vector<int> num(n);
+    for(int i=1;i<n-1;i++){
+        num[i];
+    }
+    vector<int> arr(n-1);
+    cout<<"enter the elements of the array: ";
+    for(int i=0;i<n-1;i++){
+        cin>>arr[i];
+    }
+    Solution sol;
+    int output=sol.missingNo(num,arr,n);
+    if(output!= -1){
+        cout<<"the missing no from the array is: "<<output;
+    }
+    else{
+        cout<<"there is no missing element.";
+    }
+}
