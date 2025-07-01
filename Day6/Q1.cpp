01e6ec61de2ad228acd3bbfb331cfc8c44ff31e2
nishant <nishantsharma30305@gmail.com>
2025-07-01 21:58:32 +0530

    Update Q1.cpp

--- Day6/Q1.cpp
+++ Day6/Q1.cpp
@@ -1,6 +1,5 @@
 //Problem Statement: Given an integer N and an array of size N-1 containing N-1 numbers between 1 to N. 
 //Find the number(between 1 to N), that is not present in the given array.
-
 #include<iostream>
 #include<vector>
 using namespace std;
